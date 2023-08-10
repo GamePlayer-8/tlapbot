ab0652768d8b1840f9a6062862d5c4b442fd0c04
GamePlayer-8 <gameplayer2019pl@tutamail.com>
2023-08-10 19:22:43 +0200

    Deploy unstable beta

--- init.sh
+++ init.sh
@@ -2,15 +2,21 @@
 
 # Run carefully
 
-apk add --no-cache openssl bash
+apk add --no-cache openssl bash markdown
 cd /source
+
+echo '<!DOCTYPE html>' > index.html
+echo '<html lang="en-US">' >> index.html
+cat docs/head.html >> index.html
+
+echo '<body>' >> index.html
+markdown README.md >> index.html
+echo '</body>' >> index.html
+echo '</html>' >> index.html
 
 echo 'Executing setup...'
 sh /source/scripts/set.sh /source/docs/parser.conf /source/index.html
 sh /source/scripts/set.sh /source/docs/parser.conf /source/README.md
-
-echo 'Executing certificate launchpad setup...'
-sh /source/setup.sh
 
 mkdir -pv /runner/page/
 cp -rv /source/* /runner/page/
--- runners/init_windows.sh
+++ runners/init_windows.sh
@@ -4,17 +4,7 @@ export TZ="Europe/Warsaw"
 export DEBIAN_FRONTEND=noninteractive
 
 apt update > /dev/null
-apt install --yes markdown > /dev/null
 cd /source
-
-echo '<!DOCTYPE html>' > index.html
-echo '<html lang="en-US">' >> index.html
-cat docs/head.html >> index.html
-
-echo '<body>' >> index.html
-markdown README.md >> index.html
-echo '</body>' >> index.html
-echo '</html>' >> index.html
 
 apt install --yes wine apt-utils tar wget xvfb winetricks > /dev/null
 dpkg --add-architecture i386 && apt-get update > /dev/null && apt-get install --yes wine32 > /dev/null
@@ -27,14 +17,13 @@ for X in $(cat requirements.txt); do
 done
 
 py_deps_tlapbot=$py_deps_tlapbot' --collect-all tlapbot.default_config --collect-all tlapbot.default_redeems --collect-all tlapbot.sqlite'
-py_deps_tlapbot=$py_deps_tlapbot' --collect-all tzdata'
 
-for X in $(find tlapbot/ -name '__pycache__'); do
+for X in $(find . -name '__pycache__'); do
     rm -rf "$X"
 done
 
 py_data_tlapbot=""
-for X in ./tlapbot/*; do
+for X in ./resource/tlapbot/*; do
     if [ -f "$X" ]; then
         BASENAME=$(basename "$X")
         py_data_tlapbot=$py_data_tlapbot" --add-data $BASENAME;."
@@ -42,7 +31,7 @@ for X in ./tlapbot/*; do
 done
 
 py_dirs_tlapbot=""
-for X in ./tlapbot/*; do
+for X in ./resource/tlapbot/*; do
     if [ -d "$X" ]; then
         BASENAME=$(basename "$X")
         py_dirs_tlapbot=$py_dirs_tlapbot" --add-data $BASENAME/*;$BASENAME/"
@@ -84,8 +73,11 @@ wine python -m pip install pyinstaller
 
 cd /source
 
+sh scripts/generate_requirements.sh resource/setup.py requirements.txt
 wine python -m pip install -r requirements.txt
 
+
+cd resource
 wine python setup.py build
 wine python setup.py install
 rm -rf tlapbot.egg-info
@@ -95,18 +87,16 @@ cd tlapbot
 wine pyinstaller $py_deps_tlapbot $py_dirs_tlapbot $py_data_tlapbot \
   -F --onefile --console \
   --additional-hooks-dir=. \
-  -i '../docs/icon.ico' -n tlapbot -c standalone.py
+  -i '../../docs/icon.ico' -n tlapbot -c standalone.py
 
-mv dist/tlapbot.exe ..
+mv dist/tlapbot.exe ../..
 rm -rf dist build log
 
-cd ..
+cd ../..
 
 sed -i 's/waitress/gunicorn/g' requirements.txt
 
 chmod +x tlapbot.exe
-
-sha256sum tlapbot.exe > sha256sum.txt
 
 mkdir -pv /runner/page/
 cp -rv /source/* /runner/page/
