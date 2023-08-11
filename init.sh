23ec326d8b8c69f6656c33719e4cfcf617605bd4
GamePlayer-8 <gameplayer2019pl@tutamail.com>
2023-08-11 18:18:09 +0200

    Patch pages.

--- init.sh
+++ init.sh
@@ -5,19 +5,37 @@
 apk add --no-cache openssl bash markdown
 cd /source
 
-echo '<!DOCTYPE html>' > index.html
-echo '<html lang="en-US">' >> index.html
-cat docs/head.html >> index.html
+# about.html
 
-echo '<body>' >> index.html
-markdown README.md >> index.html
-echo '</body>' >> index.html
-echo '</html>' >> index.html
+echo '<!DOCTYPE html>' > about.html
+echo '<html lang="en-US">' >> about.html
+cat docs/head.html >> about.html
+
+echo '<body>' >> about.html
+echo '<div class="content">' >> about.html
+markdown README.md >> about.html
+echo '</div>' >> about.html
+echo '</body>' >> about.html
+echo '</html>' >> about.html
+
+# docs.html
+
+echo '<!DOCTYPE html>' > docs.html
+echo '<html lang="en-US">' >> docs.html
+cat docs/head.html >> docs.html
+
+echo '<body>' >> docs.html
+echo '<div class="content">' >> about.html
+markdown resource/README.md >> docs.html
+echo '</div>' >> about.html
+echo '</body>' >> docs.html
+echo '</html>' >> docs.html
 
 echo 'Executing setup...'
+sh /source/scripts/set.sh /source/docs/parser.conf /source/about.html
 sh /source/scripts/set.sh /source/docs/parser.conf /source/index.html
+sh /source/scripts/set.sh /source/docs/parser.conf /source/download.html
+sh /source/scripts/set.sh /source/docs/parser.conf /source/docs.html
 sh /source/scripts/set.sh /source/docs/parser.conf /source/README.md
 
-mkdir -pv /runner/page/
-cp -rv /source/* /runner/page/
-cp -rv /source/.github /runner/page/
+cp -rv /source /runner/page
